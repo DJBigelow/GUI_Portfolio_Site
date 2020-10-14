59d4cd47f6e12bb683f4966242e693c6557bfbe5
djbigelow0@gmail.com <djbigelow0@gmail.com>
2020-10-14 13:42:28 -0600

    Revert "Deleted Projects.cs since the ProjectList component has made it redundant"
    This reverts commit 9ce5e0f963329e165e7578660ae119ca4f8e1fdc.

---
+++ Portfolio.WASM/Pages/Projects.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using Portfolio.Shared;
+using Portfolio.Shared.Models;
+using Portfolio.WASM.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Portfolio.WASM.Pages
+{
+    public partial class Projects
+    {
+        [Inject]
+        public IProjectDataService ProjectDataService{ get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            ProjectList = await ProjectDataService.GetProjectsAsync();
+             
+        }
+
+        [Parameter]
+        public IEnumerable<ProjectViewModel> ProjectList { get; set; }
+
+    }
+}
