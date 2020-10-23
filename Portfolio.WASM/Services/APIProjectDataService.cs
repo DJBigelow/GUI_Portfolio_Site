176e55ab16f4d282d10d15a32bccc5580721c16e
DJ Bigelow <dj.bigelow@students.snow.edu>
2020-10-22 20:08:14 -0600

    Followed through entire tutorial. API project is busted.

--- Portfolio.WASM/Services/APIProjectDataService.cs
+++ Portfolio.WASM/Services/APIProjectDataService.cs
@@ -7,16 +7,20 @@ using System.Net.Http;
 using Portfolio.Shared.Models;
 using System.Text;
 using Portfolio.Shared;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using System.Net.Http.Headers;
 
 namespace Portfolio.WASM.Services
 {
     public class APIProjectDataService : IProjectDataService
     {
         private readonly HttpClient httpClient;
+        private readonly IAccessTokenProvider tokenProvider;
 
-        public APIProjectDataService(HttpClient httpClient)
+        public APIProjectDataService(HttpClient httpClient, IAccessTokenProvider tokenProvider)
         {
             this.httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+            this.tokenProvider = tokenProvider;
         }
 
 
@@ -24,12 +28,12 @@ namespace Portfolio.WASM.Services
         {
             var response = await httpClient.GetStreamAsync("api/project/getprojects");
 
-
             var projectVMs = await JsonSerializer.DeserializeAsync<IEnumerable<ProjectViewModel>>(
                 response, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
 
             return projectVMs;
         }
+
 
         public async Task<ProjectViewModel> GetProjectAsync(int projectID)
         {
@@ -38,6 +42,7 @@ namespace Portfolio.WASM.Services
 
             return projectVM;
         }
+
 
         public async Task<ProjectViewModel> GetProjectAsync(string projectSlug)
         {
@@ -50,6 +55,9 @@ namespace Portfolio.WASM.Services
 
         public async Task AddProjectAsync(ProjectViewModel projectVM)
         {
+            var token = await TryGetAccessToken();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Value);
+
             Project project = new Project(projectVM);
 
             var projectJson = new StringContent(JsonSerializer.Serialize(project), Encoding.UTF8, "application/json");
@@ -59,26 +67,50 @@ namespace Portfolio.WASM.Services
 
         public async Task DeleteProjectAsync(int projectID)
         {
+            var token = await TryGetAccessToken();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Value);
+
             await httpClient.DeleteAsync($"api/project/deleteproject/{projectID}");
+           
         }
 
         public async Task UpdateProjectAsync(ProjectViewModel projectVM)
         {
+            var token = await TryGetAccessToken();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Value);
+
             Project project = new Project(projectVM);
 
             var projectJson = new StringContent(JsonSerializer.Serialize(project), Encoding.UTF8, "application/json");
 
             await httpClient.PutAsync("api/project/updateproject", projectJson);
+         
         }
 
 
         public async Task AssociateProjectWithCategory(AssociationRequest associationRequest)
         {
+            var token = await TryGetAccessToken();
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Value);
+
             var json = new StringContent(JsonSerializer.Serialize(associationRequest), Encoding.UTF8, "application/json");
 
             await httpClient.PostAsync("api/project/associate", json);
+            
         }
 
 
+        private async Task<AccessToken> TryGetAccessToken()
+        {
+            var tokenResult = await tokenProvider.RequestAccessToken();
+            if (tokenResult.TryGetToken(out var token))
+            {
+                return token;
+            }
+            else
+            {
+                throw new Exception("API access not granted");
+            }
+        }
     }
 }
